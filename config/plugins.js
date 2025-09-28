8285b8ba2ff47648fe8861445f90b59f70271457
rkoenigl <rkl@searchmind.dk>
2025-09-28 15:19:24 +0200

    refactor: remove third-party import/export plugin configuration (will rely on native CLI transfer)

--- config/plugins.js
+++ config/plugins.js
@@ -3,12 +3,4 @@ module.exports = () => ({
   seo: {
     enabled: true,
   },
-  // Import Export Plugin (Strapi 5) configuration
-  'strapi-import-export': {
-    enabled: true,
-    config: {
-      // Needed for generating absolute URLs in exported data if behind a reverse proxy / cloud
-  serverPublicHostname: process.env.PUBLIC_URL || 'https://trusty-dance-ed00d6b3f6.strapiapp.com/',
-    },
-  },
 });
