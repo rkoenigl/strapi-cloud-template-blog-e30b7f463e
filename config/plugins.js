dbf55b451e85a63c453e839ff3c127f60109ace7
rkoenigl <rkl@searchmind.dk>
2025-09-28 19:40:33 +0200

    Remove export and import

--- config/plugins.js
+++ config/plugins.js
@@ -3,8 +3,4 @@ module.exports = () => ({
   seo: {
     enabled: true,
   },
-  // Export/Import KKM Plugin - Excel-based export/import for Strapi 5
-  'export-import-kkm': {
-    enabled: true,
-  },
 });
