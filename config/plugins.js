0cd5b8d75ed8c3ab5a90e50c640dc8dc597d9d19
rkoenigl <rkl@searchmind.dk>
2025-09-28 14:57:18 +0200

    feat: add and enable strapi-import-export plugin (Strapi v5)

--- config/plugins.js
+++ config/plugins.js
@@ -3,4 +3,12 @@ module.exports = () => ({
   seo: {
     enabled: true,
   },
+  // Import Export Plugin (Strapi 5) configuration
+  'strapi-import-export': {
+    enabled: true,
+    config: {
+      // Needed for generating absolute URLs in exported data if behind a reverse proxy / cloud
+      serverPublicHostname: process.env.PUBLIC_URL || '',
+    },
+  },
 });
